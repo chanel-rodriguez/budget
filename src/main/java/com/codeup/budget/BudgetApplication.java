bfa7291cc372e9cd3d254192a2abb6e73e18ca1a
Chanel Rodriguez <13car92@gmail.com>
2018-05-22 22:52:09 -0400

    first commit

---
+++ src/main/java/com/codeup/budget/BudgetApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+}
